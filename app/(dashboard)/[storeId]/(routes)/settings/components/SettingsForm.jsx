4063eafbf3876e69a256e23ac5a2c13eced95ed8
bzaman-tk <bzaman.tk@gmail.com>
2023-11-18 15:40:28 +0600

    SettingsFrom Layout Done

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.jsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.jsx
@@ -1,16 +1,76 @@
 'use client'
-
+import * as z from 'zod'
 import Heading from "@/components/ui/Heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Trash } from "lucide-react";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 
-
+const formSchema = z.object({
+    name: z.string().min(1)
+})
 
 const SettingsForm = ({ initialData }) => {
+    const [open, setOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
 
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData,
+    })
+
+    const onSubmit = data => {
+        console.log(data)
+    }
 
     return (
-        <div className="flex items-center justify-between">
-            <Heading title="Store Settings" description="Manage Store Preferences" />
-        </div>
+        <>
+            <div className="flex items-center justify-between">
+                <Heading title="Store Settings" description="Manage Store Preferences" />
+                <Button
+                    disabled={loading}
+                    variant="destructive"
+                    size="icon"
+                    onClick={() => setOpen(true)}
+                >
+                    <Trash className="h-4 w-4" />
+                </Button>
+            </div>
+            <Separator />
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8 w-full'>
+                    <div className="grid grid-cols-3 gap-8">
+                        <FormField
+                            contron={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input disabled={loading} placeholder="Store Name" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <Button disabled={loading} className="me-0 block" type="submit">
+                        Save Changes
+                    </Button>
+                </form>
+            </Form>
+        </>
     );
 };
 
