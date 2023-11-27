b694df4bf6b3565bab886acd3c33f1c542e5be75
bzaman-tk <bzaman.tk@gmail.com>
2023-11-27 16:40:16 +0600

    product cell action done

--- app/(dashboard)/[storeId]/(routes)/products/components/ProductClient.jsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/ProductClient.jsx
@@ -26,7 +26,7 @@ const ProductClient = ({ data }) => {
             </div>
 
             <Separator />
-            <DataTable searchKey="label" columns={columns} data={data} />
+            <DataTable searchKey="name" columns={columns} data={data} />
             <Heading title="API" description="API calls for Products" />
             <Separator />
             <ApiList entityName="products" entityIdName="productId" />
